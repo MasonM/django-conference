30738f53553bbc1cc690ceeb8342c06960b47864
mrman@masons-comp <mrman@masons-comp>
2009-07-13 03:01:53 -0400

    Adding example project.

---
+++ example_project/urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls.defaults import *
+from django.conf import settings
+from django.contrib import admin
+
+
+admin.autodiscover()
+
+
+urlpatterns = patterns('',
+    (r'^admin/(.*)', admin.site.root),
+    (r'^admin_docs/', include('django.contrib.admindocs.urls')),
+    (r'^conference/', include('django_conference.urls')),
+    (r'^accounts/login/', 'django.contrib.auth.views.login', {
+        'template_name': 'admin/login.html'
+    }),
+)
+
+#allow static serve only for development work 
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+            'show_indexes': True}),
+    )
