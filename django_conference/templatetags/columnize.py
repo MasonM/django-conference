23f598bda0898b3db13617cc08f20fd9d7ee82c2
Mason Malone <mason.malone@gmail.com>
2012-09-16 21:16:35 -0400

    Add columnize helper to assist in formatting plain-text e-mails

---
+++ django_conference/templatetags/columnize.py
@@ -0,0 +1,43 @@
+from django.template.defaultfilters import stringfilter
+from django import template
+
+import textwrap
+
+register = template.Library()
+
+@register.filter
+@stringfilter
+def columnize(textblock, width):
+    """
+    Simple helper to take in a plain-text definition list delimited by colons
+    and add formats it nicely.
+
+    Example input:
+col1: foo
+a really long term: bar
+l: baz
+    
+    Output of columnize:10
+col1:       foo
+
+a really  
+long term:  bar
+
+l:          baz
+    """
+    lines = unicode(textblock).split("\n")
+    return columnize_helper(lines, width)
+columnize.is_safe = True
+
+def columnize_helper(lines, width):
+    out = ""
+    for line in lines:
+        if ":" in line:
+            label, value = line.split(':', 1)
+            label_lines = textwrap.wrap(label, width)
+            label_lines[-1] += ':'
+            out += columnize_helper(label_lines[:-1], width)
+            out += "%s %s\n\n" % (label_lines[-1].ljust(width), value)
+        else:
+            out += line.ljust(width) + "\n"
+    return out
