58e818fd24073ef46bd4b976ba8e904a644ba41a
Mason Malone <mason.malone@gmail.com>
2015-08-17 20:21:15 -0400

    Fix money_format() filter so it isn't dependent on locale

--- django_conference/templatetags/money_format.py
+++ django_conference/templatetags/money_format.py
@@ -1,9 +1,7 @@
-import locale
 from decimal import Decimal
 
 from django import template
 
-locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 register = template.Library()
 
 @register.filter
@@ -11,5 +9,24 @@ def money_format(value):
     """
     Display float in money-style format with specified decimal places and
     comma-grouped thousands.
+    Adapted from https://docs.python.org/2/library/decimal.html#recipes
     """
-    return locale.currency(Decimal(str(value)), grouping=True)
+    q = Decimal(10) ** -2      # 2 places --> '0.01'
+    _, digits, exp = Decimal(str(value)).quantize(q).as_tuple()
+    result = []
+    digits = map(str, digits)
+    build, next = result.append, digits.pop
+    for i in range(2):
+        build(next() if digits else '0')
+    build('.')
+    if not digits:
+        build('0')
+    i = 0
+    while digits:
+        build(next())
+        i += 1
+        if i == 3 and digits:
+            i = 0
+            build(',')
+    build('$')
+    return ''.join(reversed(result))
