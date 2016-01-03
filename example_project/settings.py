25be88e84243d8a940a420ad2ee39ad752c71549
Mason Malone <mason.malone@gmail.com>
2016-01-02 22:46:49 -0500

    Django 1.2 compatibility

--- example_project/settings.py
+++ example_project/settings.py
@@ -10,12 +10,16 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'conference'          # Or path to database file if using sqlite3.
-DATABASE_USER = 'conferenceUser'      # Not used with sqlite3.
-DATABASE_PASSWORD = 'conferencePass'  # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASES = {
+    "default": {
+        "ENGINE": 'django.db.backends.mysql',
+        "NAME": 'conference',
+        "USER": 'conferenceUser',
+        "PASSWORD": 'conferencePass',
+        "HOST": '',
+        "PORT": '',
+    },
+}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
