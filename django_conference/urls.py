7799f4ebbbd3f12760c0ff1f91f8243330788aa8
Mason Malone <mason.malone@gmail.com>
2016-05-02 20:34:55 -0400

    Clean up urls.py and organize it into sections

--- django_conference/urls.py
+++ django_conference/urls.py
@@ -5,37 +5,42 @@ from django_conference import views, autocomplete, admin_tasks
 
 
 urlpatterns = patterns('',
-    url(r'edit_paper/(?P<paper_id>\d+)',
-        views.edit_paper,
-        name="django_conference_edit_paper"),
-    url(r'submit_paper',
+    # Paper submission/editing
+    url(r'^submit_paper',
         views.submit_paper,
         name="django_conference_submit_paper"),
-    url(r'submit_session_papers',
-        views.submit_session_papers,
-        name="django_conference_submit_session_papers"),
-    url(r'submit_session',
-        views.submit_session,
-        name="django_conference_submit_session"),
+    url(r'^edit_paper/(?P<paper_id>\d+)',
+        views.edit_paper,
+        name="django_conference_edit_paper"),
     url(r'^submit_success/(?P<id>\d+)/',
         views.submission_success,
         name="django_conference_submission_success"),
 
+    # Session submission
+    url(r'^submit_session_papers',
+        views.submit_session_papers,
+        name="django_conference_submit_session_papers"),
+    url(r'^submit_session',
+        views.submit_session,
+        name="django_conference_submit_session"),
+
+    # Meeting registration
+    url(r'^payment/(?P<reg_id>\d+)?',
+        views.payment,
+        name="django_conference_payment"),
     url(r'^register_success',
         TemplateView.as_view(template_name=
             'django_conference/register_success.html'),
         name="django_conference_register_success"),
-    url('^paysuccess',
+    url(r'^register',
+        views.register,
+        name="django_conference_register"),
+    url(r'^paysuccess',
         TemplateView.as_view(template_name=
             'django_conference/pay_success.html'),
         name="django_conference_paysuccess"),
-    url(r'^payment/(?P<reg_id>\d+)?',
-        views.payment,
-        name="django_conference_payment"),
-    url(r'register',
-        views.register,
-        name="django_conference_register"),
 
+    # Admin
     url(r'^choose_admin_task/(?P<meeting_id>\d+)',
         admin_tasks.choose_task,
         name="django_conference_choose_admin_task"),
@@ -52,6 +57,7 @@ urlpatterns = patterns('',
         autocomplete.PaperAutocomplete.as_view(),
         name='paper-autocomplete'),
 
+    # Homepage
     url(r'',
         views.homepage,
         name="django_conference_home"),
